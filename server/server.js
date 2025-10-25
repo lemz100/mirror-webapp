026b6f16abaeebee2450fb6793ad05c2c9fedc40
lemz100 <lemuelpoku22@gmail.com>
2025-10-25 22:50:06 +0100

    AUTH 95% Done - Working DB + Backend connected

--- server/server.js
+++ server/server.js
@@ -2,20 +2,30 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const { loadUsers, saveUsers } = require('./scripts/auth'); // Imports JSON methods from auth.js
 const app = express();
+const pool = require('./data/db');
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from our server!');
-});
+async function testConnection() {
+  try {
+    const res = await pool.query('SELECT NOW()'); // Gets the current date - test query to see if connection is live.
+    console.log('Database connected: ', res.rows[0]);
+  } catch (err) {
+    console.error('Database connection error:', err);
+  }
+}
 
 // To distinguish input.
 function validateEmail(value) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(value);
 }
+
+app.get('/', (req, res) => {
+  res.send('Hello from our server!');
+});
 
 app.post('/signup', async (req, res) => {
   const { fname, lname, username, email, password } = req.body; // Destructures submitted form data
@@ -28,19 +38,27 @@ app.post('/signup', async (req, res) => {
     return res.status(400).json({ message: 'Email is invalid' });
   }
 
-  const users = loadUsers(); // Loads users up
   const takenFields = {}; // For taken fields.
   const saltRounds = 10; // Standard value for salting
 
-  // Gets the id of the last user in the database (for dummy testing)
-  const lastId = users.length > 0 ? users[users.length - 1].id : 0;
-
   // Checks if email may match with a user in the database
-  if (users.find((user) => user.email === email)) {
-    takenFields.email = true;
-  }
-  if (users.find((user) => user.username === username)) {
-    takenFields.username = true;
+  const query1 = await pool.query(
+    'SELECT * FROM users WHERE email = $1 OR username = $2',
+    [email, username]
+  );
+  if (query1.rows.length > 0) {
+    let arr = [...query1.rows];
+    arr.forEach((user) => {
+      let unameValid = user.username === username;
+      let emailValid = user.email === email;
+
+      if (unameValid) {
+        takenFields.username = true;
+      }
+      if (emailValid) {
+        takenFields.email = true;
+      }
+    });
   }
   // If taken fields object isn't empty, then there are fields that are taken. Returns an error.
   if (Object.keys(takenFields).length !== 0) {
@@ -52,17 +70,12 @@ app.post('/signup', async (req, res) => {
 
   // User is recorded to be added to the database
   const name = fname + ' ' + lname;
-  const newUser = {
-    id: lastId + 1,
-    name,
-    username,
-    email,
-    password: hashedPassword,
-  };
+
   // User added to the database
-  users.push(newUser);
-  // Database saved
-  saveUsers(users);
+  const insertQuery = await pool.query(
+    'INSERT INTO users (name, username, email, password) VALUES ($1, $2, $3, $4) RETURNING *;',
+    [name, username, email, hashedPassword]
+  );
 
   // Sends a response back to the client
   res.status(201).json({ message: 'User created successfully' });
@@ -70,7 +83,6 @@ app.post('/signup', async (req, res) => {
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body; // Gets the data passed in from request (from the form in this case)
-  const users = loadUsers();
   const input = username.trim(); // Normalizes username input
 
   // Prevents bad actors
@@ -82,9 +94,27 @@ app.post('/login', async (req, res) => {
   // If input is an email
   // Checks for matches with a user in the database
   if (validateEmail(input)) {
-    user = users.find((user) => user.email === input);
+    const query = await pool.query('SELECT * FROM users WHERE email = $1', [
+      input,
+    ]);
+    let arr = [...query.rows][0]; // Destructures the user object from the query
+    user = {
+      name: arr.name,
+      username: arr.username,
+      email: arr.email,
+      password: arr.password,
+    };
   } else {
-    user = users.find((user) => user.username === input);
+    const query = await pool.query('SELECT * FROM users WHERE username = $1', [
+      input,
+    ]);
+    let arr = [...query.rows][0]; // Destructures the user object from the query
+    user = {
+      name: arr.name,
+      username: arr.username,
+      email: arr.email,
+      password: arr.password,
+    };
   }
 
   if (!user) {
@@ -106,3 +136,5 @@ app.post('/login', async (req, res) => {
 app.listen(8080, () => {
   console.log('server listening on port 8080');
 });
+
+testConnection();
